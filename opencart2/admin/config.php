093bc83c41e84b6cb5206ecdb1d338972a417170
roockee <roockee@163.com>
2015-04-09 14:38:03 +0800

    Test Git commit

---
+++ opencart2/admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart2/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart2/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart2/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\opencart2/admin/');
+define('DIR_SYSTEM', 'D:\www\opencart2/system/');
+define('DIR_LANGUAGE', 'D:\www\opencart2/admin/language/');
+define('DIR_TEMPLATE', 'D:\www\opencart2/admin/view/template/');
+define('DIR_CONFIG', 'D:\www\opencart2/system/config/');
+define('DIR_IMAGE', 'D:\www\opencart2/image/');
+define('DIR_CACHE', 'D:\www\opencart2/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\opencart2/system/download/');
+define('DIR_UPLOAD', 'D:\www\opencart2/system/upload/');
+define('DIR_LOGS', 'D:\www\opencart2/system/logs/');
+define('DIR_MODIFICATION', 'D:\www\opencart2/system/modification/');
+define('DIR_CATALOG', 'D:\www\opencart2/catalog/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart2');
+define('DB_PREFIX', 'oc_');
---
+++ opencart2/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart2/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart2/');
+
+// DIR
+define('DIR_APPLICATION', 'D:\www\opencart2/catalog/');
+define('DIR_SYSTEM', 'D:\www\opencart2/system/');
+define('DIR_LANGUAGE', 'D:\www\opencart2/catalog/language/');
+define('DIR_TEMPLATE', 'D:\www\opencart2/catalog/view/theme/');
+define('DIR_CONFIG', 'D:\www\opencart2/system/config/');
+define('DIR_IMAGE', 'D:\www\opencart2/image/');
+define('DIR_CACHE', 'D:\www\opencart2/system/cache/');
+define('DIR_DOWNLOAD', 'D:\www\opencart2/system/download/');
+define('DIR_UPLOAD', 'D:\www\opencart2/system/upload/');
+define('DIR_MODIFICATION', 'D:\www\opencart2/system/modification/');
+define('DIR_LOGS', 'D:\www\opencart2/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart2');
+define('DB_PREFIX', 'oc_');
